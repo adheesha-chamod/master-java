52331e7f13b4544bd339c0aae0ba3df70a9f2a5e
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2025-06-30 00:03:23 +0530

    Enhance incrementTotalBytes method documentation to clarify non-atomic operation steps

--- part3/6_concurrency_&_multi_threading/6_race_condition/DownloadStatus.java
+++ part3/6_concurrency_&_multi_threading/6_race_condition/DownloadStatus.java
@@ -6,6 +6,13 @@ class DownloadStatus {
     }
 
     public void incrementTotalBytes() {
+        /**
+         * Non-atomic operation
+         * Involves 3 steps:
+         *  - Load the value to the CPU from main memory
+         *  - Increment the value by 1
+         *  - Write the updated value to the main memory
+         */
         totalBytes++;
     }
 }
